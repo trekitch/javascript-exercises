b9214efe2a238d37afd2dcf90e791d23569285b6
trekitch <trekitch@ymail.com>
2022-01-08 20:34:46 -0500

    finished first 2 tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word, number) {
+  let returnString = ""
+  if(number >= 0){
+    for(let i = 0; i < number; i++){
+      returnString += word;
+    }
+      return returnString;
+  }else{
+    return "ERROR"
+  }
 
 };
 
