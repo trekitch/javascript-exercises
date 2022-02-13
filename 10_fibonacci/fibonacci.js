a47babbd33fc91d905a8d55a8d02201f5e71b62d
trekitch <trekitch@ymail.com>
2022-02-13 02:36:28 -0500

    finished exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(i) {
+    if(i < 0) return "OOPS";
+    let fib = [];
+    fib[0] = 0;
+    fib[1] = 1
 
+    for (let index = 2; index < 50; index++) {
+        fib[index] = (fib[index-1] + fib[index-2]);           
+    }
+    return fib[i];
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
